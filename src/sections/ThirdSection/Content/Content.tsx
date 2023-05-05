329a59bedcd77fe579aca3c51756b3dc9d860ce5
Timur Ermolaev <timkogas@gmail.com>
2023-05-05 16:36:57 +0300

    refactor

---
+++ src/sections/ThirdSection/Content/Content.tsx
@@ -0,0 +1,39 @@
+import styles from './Content.module.css'
+import { observer } from 'mobx-react';
+import BYInfo from '../../../assets/images/section-three-BR-info.png'
+import State from '../../../store/State';
+export const Content = observer(() => {
+
+  const index = State.getActiveCountryIndex()
+
+  const stylesActiveContent = index !== -1 ? {
+    transform: 'translate(calc(100%/4.7))',
+    transition: '0.5s all ease;',
+    transitionDelay: '0.2s',
+    zIndex: 1
+  } : {}
+  
+  return (
+    <div className={styles.content} style={stylesActiveContent}>
+      <div className={styles.title}>
+        смотреть кино бесплатно и без смс
+      </div>
+
+      <div className={styles.info}>
+        <div className={styles.info_block}>
+          <p className={styles.info_title}>Беларусь</p>
+          <p className={styles.info_text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis eros commodo, vehicula turpis eu, posuere diam. Duis et dapibus lacus. Morbi bibendum, nulla sed imperdiet lacinia, diam nibh mattis sapien, non vehicula nulla nisl et nibh. Sed ac tincidunt augue. Suspendisse dictum pulvinar purus sed eleifend. Sed eu suscipit nulla. Sed nec risus eget justo dapibus cursus in et nibh.  Mauris nec diam at leo pellentesque dignissim et eu augue. Nunc in mattis nibh, et laoreet arcu. Aenean sit amet rhoncus risus. Etiam vitae pellentesque diam, nec porttitor eros. Aliquam vehicula diam a nisi sodales, sit amet feugiat dui vulputate. Vestibulum pulvinar ligula rutrum eros laoreet pellentesque. Aliquam </p>
+          <p className={styles.info_text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis eros commodo, vehicula turpis eu, posuere diam. Duis et dapibus lacus. Morbi bibendum, nulla sed imperdiet lacinia, diam nibh mattis sapien, non vehicula nulla nisl et nibh. Sed ac tincidunt augue. Suspendisse dictum pulvinar purus sed eleifend. Sed eu suscipit nulla. Sed nec risus eget justo dapibus cursus in et nibh.  Mauris nec diam at leo pellentesque dignissim et eu augue. Nunc in mattis nibh, et laoreet arcu. Aenean sit amet rhoncus risus. Etiam vitae pellentesque diam, nec porttitor eros. Aliquam vehicula diam a nisi sodales, sit amet feugiat dui vulputate. Vestibulum pulvinar ligula rutrum eros laoreet pellentesque. Aliquam </p>
+          <div className={styles.info_btn}>
+            смотреть фильм
+          </div>
+        </div>
+        <div>
+          <img className={styles.info_img} src={BYInfo} alt='country' />
+        </div>
+      </div>
+    </div>
+  )
+});
+
+export default Content
--- src/sections/ThirdSection/ThirdSection.tsx
+++ src/sections/ThirdSection/ThirdSection.tsx
@@ -5,12 +5,9 @@ import IN from '../../assets/images/section-three-IN.png'
 import CN from '../../assets/images/section-three-CN.png'
 import CS from '../../assets/images/section-three-CS.png'
 import ZA from '../../assets/images/section-three-ZA.png'
-import BYInfo from '../../assets/images/section-three-BR-info.png'
 import CountryItem from './CountryItem/CountryItem'
-import { useState, useCallback, useEffect } from "react";
-import { observer } from 'mobx-react-lite'
-import State from '../../store/State'
 import Footer from './Footer/Footer'
+import Content from './Content/Content'
 
 const countries = [
   { img: BY, name: "беларусь" },
@@ -20,19 +17,8 @@ const countries = [
   { img: CS, name: "сербия" },
   { img: ZA, name: "юар" },
 ]
-let stylesActiveContent: any
 
-export const ThirdSection = observer(() => {
-
-  const index = State.getActiveCountryIndex()
-
-  stylesActiveContent = index !== -1 ? {
-    transform: 'translate(calc(100%/4.7))',
-    transition: '0.5s all ease;',
-    transitionDelay: '0.2s',
-    zIndex: 1
-  } : {}
-
+export const ThirdSection = () => {
 
   return (<>
     <div className={"section " + styles.section} id='section3'>
@@ -49,32 +35,13 @@ export const ThirdSection = observer(() => {
           />
         )
       })}
-
+      <Content/>
       <div className={styles.hr}></div>
 
-      <div className={styles.content} style={stylesActiveContent}>
-        <div className={styles.title}>
-          смотреть кино бесплатно и без смс
-        </div>
 
-        <div className={styles.info}>
-          <div className={styles.info_block}>
-            <p className={styles.info_title}>Беларусь</p>
-            <p className={styles.info_text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis eros commodo, vehicula turpis eu, posuere diam. Duis et dapibus lacus. Morbi bibendum, nulla sed imperdiet lacinia, diam nibh mattis sapien, non vehicula nulla nisl et nibh. Sed ac tincidunt augue. Suspendisse dictum pulvinar purus sed eleifend. Sed eu suscipit nulla. Sed nec risus eget justo dapibus cursus in et nibh.  Mauris nec diam at leo pellentesque dignissim et eu augue. Nunc in mattis nibh, et laoreet arcu. Aenean sit amet rhoncus risus. Etiam vitae pellentesque diam, nec porttitor eros. Aliquam vehicula diam a nisi sodales, sit amet feugiat dui vulputate. Vestibulum pulvinar ligula rutrum eros laoreet pellentesque. Aliquam </p>
-            <p className={styles.info_text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis eros commodo, vehicula turpis eu, posuere diam. Duis et dapibus lacus. Morbi bibendum, nulla sed imperdiet lacinia, diam nibh mattis sapien, non vehicula nulla nisl et nibh. Sed ac tincidunt augue. Suspendisse dictum pulvinar purus sed eleifend. Sed eu suscipit nulla. Sed nec risus eget justo dapibus cursus in et nibh.  Mauris nec diam at leo pellentesque dignissim et eu augue. Nunc in mattis nibh, et laoreet arcu. Aenean sit amet rhoncus risus. Etiam vitae pellentesque diam, nec porttitor eros. Aliquam vehicula diam a nisi sodales, sit amet feugiat dui vulputate. Vestibulum pulvinar ligula rutrum eros laoreet pellentesque. Aliquam </p>
-            <div className={styles.info_btn}>
-              смотреть фильм
-            </div>
-          </div>
-          <div>
-            <img className={styles.info_img} src={BYInfo} alt='country' />
-          </div>
-        </div>
-      </div>
-
-      <Footer/>
+      <Footer />
     </div>
   </>)
-});
+};
 
 export default ThirdSection
