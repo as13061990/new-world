4eaf804d6c728b6a6f399656bfb43b7de3a0ea24
as13061990 <as13061990@yandex.ru>
2023-05-15 17:53:17 +0700

    sphere

--- src/three/Planet.ts
+++ src/three/Planet.ts
@@ -8,27 +8,31 @@ class Planet {
 
   private _build(): void {
     const root = document.querySelector('#canvas_three') as HTMLElement;
+    
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, root.clientWidth / root.clientHeight, 0.1, 1000);
-    
+    camera.position.set(0, 0, 3);
+
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(root.clientWidth, root.clientHeight);
-    // root.appendChild(renderer.domElement);
     root.insertBefore(renderer.domElement, root.firstChild);
 
+    const lightHolder = new THREE.Group(); // Создание группы для СВЕТОВ!
+    const aLight = new THREE.DirectionalLight(0xFFFFFF, 2); // Создание простого Света!
+    aLight.position.set(-1.5, 1.7, .7); // Установка позиции для этого света
+    lightHolder.add(aLight); // Прикрепляем к удержателю позиции света, чтобы он дальше не крутился вместе с объектами на сцене
+    const aLight2 = new THREE.DirectionalLight(0xFFFFFF, 2); // Второй дополнительный свет
+    aLight2.position.set(-1.5, 0.3, .7);
+    lightHolder.add(aLight2);
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0xFF0000 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    const geomHide = new THREE.SphereGeometry(1.0499, 64, 36);
+    const matHide = new THREE.MeshStandardMaterial({ color: new THREE.Color(0x091e5a) });
+    const meshHide = new THREE.Mesh(geomHide, matHide);
+
+    scene.add(meshHide);
+    scene.add(lightHolder);
     
-    camera.position.z = 5;
-    
-    const animate = (time: number) => {
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      cube.rotation.z += 0.01;
-    
+    const animate = (time: number): void => {
       renderer.render(scene, camera);
       requestAnimationFrame(animate);
     }
