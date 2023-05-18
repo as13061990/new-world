7a673a8c45bfbd0cbed8c6ac64a2eea7bd6c717b
Timur Ermolaev <timkogas@gmail.com>
2023-05-18 11:06:59 +0300

    fix scroll

--- src/page/Main.tsx
+++ src/page/Main.tsx
@@ -6,22 +6,53 @@ import { observer } from "mobx-react-lite";
 import State from "../store/State";
 import { useCallback } from "react";
 
+function calc(t: any, b: any, c: any, d: any) {
+  t /= d / 2;
+  if (t < 1) return c / 2 * t * t + b;
+  t--;
+  return -c / 2 * (t * (t - 2) - 1) + b;
+};
+const scrollTo = (element: HTMLElement, to: number, duration: number) => {
+  const start = element.scrollTop;
+  const change = to - start;
+  let currentTime = 0;
+  const increment = 20;
+
+  const animateScroll = () => {
+    currentTime += increment;
+    const val = calc(currentTime, start, change, duration);
+    element.scrollTop = val;
+    if (currentTime < duration) {
+      setTimeout(animateScroll, increment);
+    }
+  };
+
+  animateScroll();
+};
+
+
 export const Main = observer(() => {
 
   const scroll = useCallback(() => {
     const main = document.querySelector('.main') as HTMLElement;
     const section1 = document.getElementById('section1') as HTMLElement;
-
     const scrollTop = main.scrollTop;
-
     const precent = scrollTop / (section1.scrollHeight) * 100
+    let mark = false
+    if (precent >= 99.99 && !mark) {
+      if (precent > 100) return
+      mark = true
+      const scrollPosition = main.scrollTop;
+      const oldScrollHeight = section1.scrollHeight; 
 
-    if (precent >= 100) {
-      section1.classList.add('first_section_disable');
-      section1.classList.remove('first_section_active');
-    } else if (precent === 0) {
-      section1.classList.add('first_section_active');
-      section1.classList.remove('first_section_disable');
+      section1.style.scrollMarginBottom = "100vh"; 
+      const newScrollHeight = section1.scrollHeight; 
+      const newScrollTop = (scrollPosition / oldScrollHeight) * newScrollHeight; 
+
+      main.scrollTop = newScrollTop;
+    } else if (precent <= 1) {
+      mark = false
+      section1.style.scrollMarginBottom = '15vh';
     }
     State.setScrollPrecent(precent)
   }, [])
--- src/sections/FirstSection/FirstSection.tsx
+++ src/sections/FirstSection/FirstSection.tsx
@@ -1,5 +1,4 @@
 import styles from './FirstSection.module.css'
-import './FirstSection.css'
 import earth from '../../assets/images/half-earth.png'
 import arrow from '../../assets/images/section-one-arrow.svg'
 import State from '../../store/State';
