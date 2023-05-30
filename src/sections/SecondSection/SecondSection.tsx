6a4be8b72d00482550d1fe910ff71c7142706305
Timur Ermolaev <timkogas@gmail.com>
2023-05-30 10:56:56 +0300

    fixes

--- src/sections/SecondSection/SecondSection.tsx
+++ src/sections/SecondSection/SecondSection.tsx
@@ -3,12 +3,11 @@ import bg from '../../assets/images/section-two-bg.png'
 import arrow from '../../assets/images/section-one-arrow.svg'
 import State, { MAX_STEP } from '../../store/State';
 import { observer } from 'mobx-react-lite';
-import { useEffect } from 'react';
 
 export const SecondSection = observer(() => {
   let sectionActiveClass = State.getStep() === MAX_STEP - 1 ? styles.active
     : State.getStep() > MAX_STEP - 1 ? styles.inactiveUnder : styles.inactive
-  console.log('lol')
+
   return (
     <div className={styles.section + ' ' + sectionActiveClass} id='section2'>
       <div className={styles.img_block}>
